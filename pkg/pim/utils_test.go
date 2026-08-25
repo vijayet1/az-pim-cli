e498a81587b2582b13c1b7dc77642ed244958ca7
Vijay Gupta <vijay.et1@gmail.com>
2026-08-25 08:58:10 +0200

    fix(test): calculate timezone offset for test date in TestParseDateTime

--- pkg/pim/utils_test.go
+++ pkg/pim/utils_test.go
@@ -15,15 +15,16 @@ func TestParseDateTime(t *testing.T) {
 	now := time.Now().Local()
 	currentDate := now.Format("2006-01-02")
 	currentTZ := now.Format("-07:00")
+	dateTZ := time.Date(2024, 12, 31, 0, 0, 0, 0, time.Local).Format("-07:00")
 	errMsg := "resulting startDateTime does not match expected value"
 
 	dateOnly, _ := parseDateTime("31/12/2024", "")
 	timeOnly, _ := parseDateTime("", "13:37")
 	dateTime, _ := parseDateTime("31/12/2024", "13:37")
 
-	assert.Equal(t, fmt.Sprintf("2024-12-31T00:00:00%s", currentTZ), dateOnly, errMsg)
+	assert.Equal(t, fmt.Sprintf("2024-12-31T00:00:00%s", dateTZ), dateOnly, errMsg)
 	assert.Equal(t, fmt.Sprintf("%sT13:37:00%s", currentDate, currentTZ), timeOnly, errMsg)
-	assert.Equal(t, fmt.Sprintf("2024-12-31T13:37:00%s", currentTZ), dateTime, errMsg)
+	assert.Equal(t, fmt.Sprintf("2024-12-31T13:37:00%s", dateTZ), dateTime, errMsg)
 }
 
 func TestCreateResourceAssignmentRequest(t *testing.T) {
